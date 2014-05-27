95217eb257ee1c17da801bd0c2aaa1647e814720
Andrea <info@andyosuna.com>
2014-05-26 19:12:03 -0500

    HTML/CSS - Homepage
    Started on the html from base, deleted base and added a customized
    version of bootstrap. Added the whole directory to the resources dir.
    and the min versions lie in the html dir.

---
+++ html/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Morton Casa de Subastas">
+    <meta name="author" content="Andy Osuna">
+    <link rel="shortcut icon" href="favicon.ico">
+	
+	<title>Morton Casa de Subastas - Inicio</title>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<!-- IE Support -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+	<header>
+		<h1 id="logo" class="start-brand"><a href="/">Morton Casa de Subastas</a></h1>
+		<nav class="nav start-nav">
+			<ul>
+				<li class="active"><a href="#">Inicio</a></li>
+				<li><a href="#">Subastas</a></li>
+				<li><a href="#">Departamentos</a></li>
+				<li><a href="#">Cat&aacute;logos</a></li>
+				<li><a href="#">Nosotros</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+		<div class="quick-links">
+			<a href="#">Mi Morton</a>
+			<a href="#">Ayuda</a>	
+		</div>
+	</header>
+
+	<section>
+		<h2>V&iacute;deo Destacado</h2>
+		<!-- youtube video -->
+		<p class="large">Drumstick kielbasa kevin short ribs sirloin fatback landjaeger pork chop shoulder filet mignon meatball. Pork sirloin frankfurter biltong pork loin tenderloin.</p>
+	</section>
+
+	<footer>
+		<div class="container-fluid">
+			<ul class="pull-left">
+				&copy;2014 Morton Casa de Subastas
+				<li><a href="#">T&eacute;rminos y Condiciones</a></li>
+				<li><a href="#">Aviso de Privacidad</a></li>
+				<li><a href="#">Multimedia</a></li>
+				<li><a href="#">Mi Morton</a></li>
+				<li><a href="#">Registro</a></li>
+				<li><a href="#">Ayuda</a></li>
+			</ul>
+			<!-- TODO 26/05/14 Footer Logo -->
+			<ul class="pull-right">
+				<li><a href="#">Facebook</a></li>
+				<li><a href="#">Twitter</a></li>
+				<li><a href="#">YouTube</a></li>
+				<li><a href="#">Pinterest</a></li>
+				<li><a href="#">Instagram</a></li>
+			</ul>
+		</div>
+	</footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
