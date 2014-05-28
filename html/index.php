812cb060bc7ffe91f2c42b30d8858dcac9749665
Andrea <info@andyosuna.com>
2014-05-27 19:40:02 -0500

    HTML/CSS - Homepage 2
    Added rows and columns to the content. Verifying responsiveness on
    navbar, footer and catalog section.

--- html/index.php
+++ html/index.php
@@ -4,8 +4,11 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Morton Casa de Subastas">
+    <meta name="application-name" content="Morton Casa de Subastas">
     <meta name="author" content="Andy Osuna">
+    <meta name="description" content="Morton Casa de Subastas">
+    <meta name="keywords" content="Subastas, Mexico">
+
     <link rel="shortcut icon" href="favicon.ico">
 	
 	<title>Morton Casa de Subastas - Inicio</title>
@@ -22,51 +25,91 @@
 </head>
 
 <body>
-
 	<header>
-		<h1 id="logo" class="start-brand"><a href="/">Morton Casa de Subastas</a></h1>
-		<nav class="nav start-nav">
-			<ul>
-				<li class="active"><a href="#">Inicio</a></li>
-				<li><a href="#">Subastas</a></li>
-				<li><a href="#">Departamentos</a></li>
-				<li><a href="#">Cat&aacute;logos</a></li>
-				<li><a href="#">Nosotros</a></li>
-				<li><a href="#">Contacto</a></li>
-			</ul>
+		<div id="logo" class="pull-left"><a href="/">Morton Casa de Subastas</a></div>
+		<nav class="pull-right">
+			<a href="#">Inicio</a>
+			<a href="#">Subastas</a>
+			<a href="#">Departamentos</a>
+			<a href="#">Cat&aacute;logos</a>
+			<a href="#">Nosotros</a>
+			<a href="#">Contacto</a>
 		</nav>
-		<div class="quick-links">
+
+		<div id="quick-links">
 			<a href="#">Mi Morton</a>
 			<a href="#">Ayuda</a>	
-		</div>
+		</div><!-- end #quick-links -->
 	</header>
 
-	<section>
+	<section id="video">
 		<h2>V&iacute;deo Destacado</h2>
-		<!-- youtube video -->
-		<p class="large">Drumstick kielbasa kevin short ribs sirloin fatback landjaeger pork chop shoulder filet mignon meatball. Pork sirloin frankfurter biltong pork loin tenderloin.</p>
-	</section>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-6">
+					<!-- 	youtube video -->
+				</div><!-- end .col-sm-6 -->
+				<div class="col-sm-6">
+					<p class="large">Drumstick kielbasa kevin short ribs sirloin fatback landjaeger pork chop shoulder filet mignon meatball. Pork sirloin frankfurter biltong pork loin tenderloin.</p>
+				</div><!-- end .col-sm-6 -->
+			</div><!-- end .row -->
+		</div><!-- end .container-fluid -->
+	</section><!-- end #video -->
+
+	<section id="catalogs">
+		<h2>&Uacute;ltimos Cat&aacute;logos</h2>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4 col-sm-6">
+					<div class="cat">
+						<img src="images/cat-oportunidades.jpg" alt="Subasta de Oportunidades" title="Subasta de Oportunidades">
+						<a href="#">Ver Cat&aacute;logo</a>
+					</div><!-- end .cat -->
+				</div><!-- end .col-md-4 -->
+				<div class="col-md-4 col-sm-6">
+					<div class="cat">
+						<img src="images/cat-antiguedades.jpg" alt="Subasta de Antig&uuml;edades" title="Subasta de Antig&uuml;edades">
+						<a href="#">Ver Cat&aacute;logo</a>
+					</div><!-- end .cat -->
+				</div><!-- end .col-md-4 -->
+				<div class="col-md-4 col-sm-6">
+					<div class="cat">
+						<img src="images/cat-joyeria.jpg" alt="Subasta de Relojes y Joyer&iacute;a de la Fundaci&oacute;n Dond&eacute;" title="Subasta de Relojes y Joyer&iacute;a de la Fundaci&oacute;n Dond&eacute;">
+						<a href="#">Ver Cat&aacute;logo</a>
+					</div><!-- end .cat -->
+				</div><!-- end .col-md-4 -->
+			</div><!-- end .row -->
+		</div><!-- end .container-fluid -->
+	</section><!-- end #catalogs -->
 
 	<footer>
 		<div class="container-fluid">
-			<ul class="pull-left">
-				&copy;2014 Morton Casa de Subastas
-				<li><a href="#">T&eacute;rminos y Condiciones</a></li>
-				<li><a href="#">Aviso de Privacidad</a></li>
-				<li><a href="#">Multimedia</a></li>
-				<li><a href="#">Mi Morton</a></li>
-				<li><a href="#">Registro</a></li>
-				<li><a href="#">Ayuda</a></li>
-			</ul>
-			<!-- TODO 26/05/14 Footer Logo -->
-			<ul class="pull-right">
-				<li><a href="#">Facebook</a></li>
-				<li><a href="#">Twitter</a></li>
-				<li><a href="#">YouTube</a></li>
-				<li><a href="#">Pinterest</a></li>
-				<li><a href="#">Instagram</a></li>
-			</ul>
-		</div>
+			<div class="row">
+				<div class="col-md-4 col-sm-6">
+					<ul>
+						&copy;2014 Morton Casa de Subastas<br />
+						<li class="hidden-xs"><a href="#">T&eacute;rminos y Condiciones</a></li>
+						<li class="hidden-xs"><a href="#">Aviso de Privacidad</a></li>
+						<li><a href="#">Multimedia</a></li>
+						<li class="hidden-xs"><a href="#">Mi Morton</a></li>
+						<li><a href="#">Registro</a></li>
+						<li><a href="#">Ayuda</a></li>
+					</ul>
+				</div><!-- end .col-md-4 -->
+				<div class="col-md-4 visible-md visible-lg">
+					<a href="#"><img src="images/logo-footer.png" alt="Morton Casa de Subastas" title="Morton Casa de Subastas"></a>
+				</div><!-- end .col-md-4 -->
+				<div class="col-md-4 col-sm-6">
+					<ul class="social-media">
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Twitter</a></li>
+						<li><a href="#">YouTube</a></li>
+						<li><a href="#">Pinterest</a></li>
+						<li><a href="#">Instagram</a></li>
+					</ul><!-- end .social-media -->
+				</div><!-- end .col-md-4 -->
+			</div><!-- end .row -->
+		</div><!-- end .container-fluid -->
 	</footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
