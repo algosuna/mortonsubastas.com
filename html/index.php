2ff219f8aa1106d2fcfd58f5059b6d3299e5f0a1
Andrea <info@andyosuna.com>
2014-05-29 13:36:09 -0500

    JS - Homepage 1
    Started on the javascript for the fixed navbar. Simple crud. Must find
    a way to make the jump/transition smoother.

--- html/index.php
+++ html/index.php
@@ -118,5 +118,18 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop()==0 || $(this).scrollTop()<200) {
+				$("header").removeClass();
+			} else if ($(this).scrollTop()>=200) {
+				$("header").addClass("scrolled");
+			}else{
+				$("header").removeClass();
+			};
+		})
+	})
+	</script>
 </body>
 </html>
