a795ded8ae13632f08380b7d3a4231e4a0aa75f1
Andrea <info@andyosuna.com>
2014-05-29 13:34:57 -0500

    HTML/CSS Homepage 5
    Added the header image as well as prepared the css for the fixed navbar.

--- html/index.php
+++ html/index.php
@@ -44,46 +44,49 @@
 
 	<section id="slider">
 		<img src="images/slide-1.jpg">
+		<img src="images/slide-2.jpg">
 	</section>
 
-	<section id="video" class="container-fluid">
-		<h2>V&iacute;deo Destacado</h2>
-		<div class="row">
-			<div class="col-sm-6">
-				<!-- TODO: add youtube video iframe or something -->
-				<img src="images/yt-thumbnail.jpg" alt="YouTube Video" style="margin:0 auto;width:100%">
-			</div><!-- end .col-sm-6 -->
-			<div class="col-sm-6">
-				<p class="large">Drumstick kielbasa kevin short ribs sirloin fatback landjaeger pork chop shoulder filet mignon meatball. Pork sirloin frankfurter biltong pork loin tenderloin.</p>
-			</div><!-- end .col-sm-6 -->
-		</div><!-- end .row -->
-	</section><!-- end #video -->
+	<div id="content">
+		<section id="video" class="container-fluid">
+			<h2>V&iacute;deo Destacado</h2>
+			<div class="row">
+				<div class="col-sm-6">
+					<!-- TODO: add youtube video iframe or something -->
+					<img src="images/yt-thumbnail.jpg" alt="YouTube Video" style="margin:0 auto;width:100%">
+				</div><!-- end .col-sm-6 -->
+				<div class="col-sm-6">
+					<p class="large">Drumstick kielbasa kevin short ribs sirloin fatback landjaeger pork chop shoulder filet mignon meatball. Pork sirloin frankfurter biltong pork loin tenderloin.</p>
+				</div><!-- end .col-sm-6 -->
+			</div><!-- end .row -->
+		</section><!-- end #video -->
 
-	<hr>
+		<hr>
 
-	<section id="catalogs" class="container-fluid">
-		<h2>&Uacute;ltimos Cat&aacute;logos</h2>
-		<div class="row">
-			<div class="col-md-4 col-sm-6">
-				<div class="cat">
-					<img src="images/cat-oportunidades.jpg" alt="Subasta de Oportunidades" title="Subasta de Oportunidades">
-					<a href="#">Ver Cat&aacute;logo</a>
-				</div><!-- end .cat -->
-			</div><!-- end .col-md-4 -->
-			<div class="col-md-4 col-sm-6">
-				<div class="cat">
-					<img src="images/cat-antiguedades.jpg" alt="Subasta de Antig&uuml;edades" title="Subasta de Antig&uuml;edades">
-					<a href="#">Ver Cat&aacute;logo</a>
-				</div><!-- end .cat -->
-			</div><!-- end .col-md-4 -->
-			<div class="col-md-4 col-sm-6">
-				<div class="cat">
-					<img src="images/cat-joyeria.jpg" alt="Subasta de Relojes y Joyer&iacute;a de la Fundaci&oacute;n Dond&eacute;" title="Subasta de Relojes y Joyer&iacute;a de la Fundaci&oacute;n Dond&eacute;">
-					<a href="#">Ver Cat&aacute;logo</a>
-				</div><!-- end .cat -->
-			</div><!-- end .col-md-4 -->
-		</div><!-- end .row -->
-	</section><!-- end #catalogs -->
+		<section id="catalogs" class="container-fluid">
+			<h2>&Uacute;ltimos Cat&aacute;logos</h2>
+			<div class="row">
+				<div class="col-md-4 col-sm-6">
+					<div class="cat">
+						<img src="images/cat-oportunidades.jpg" alt="Subasta de Oportunidades" title="Subasta de Oportunidades">
+						<a href="#">Ver Cat&aacute;logo</a>
+					</div><!-- end .cat -->
+				</div><!-- end .col-md-4 -->
+				<div class="col-md-4 col-sm-6">
+					<div class="cat">
+						<img src="images/cat-antiguedades.jpg" alt="Subasta de Antig&uuml;edades" title="Subasta de Antig&uuml;edades">
+						<a href="#">Ver Cat&aacute;logo</a>
+					</div><!-- end .cat -->
+				</div><!-- end .col-md-4 -->
+				<div class="col-md-4 col-sm-6">
+					<div class="cat">
+						<img src="images/cat-joyeria.jpg" alt="Subasta de Relojes y Joyer&iacute;a de la Fundaci&oacute;n Dond&eacute;" title="Subasta de Relojes y Joyer&iacute;a de la Fundaci&oacute;n Dond&eacute;">
+						<a href="#">Ver Cat&aacute;logo</a>
+					</div><!-- end .cat -->
+				</div><!-- end .col-md-4 -->
+			</div><!-- end .row -->
+		</section><!-- end #catalogs -->
+	</div><!-- #content -->
 
 	<footer class="container-fluid">
 		<div class="row">
