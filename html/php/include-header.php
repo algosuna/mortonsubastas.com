ae189bc2ea29f0492c9672616fb75b039f413785
Andrea <info@andyosuna.com>
2014-05-30 17:41:43 -0500

    Dropdown
    Added the drop down and iz pretty :)

--- html/php/include-header.php
+++ html/php/include-header.php
@@ -3,13 +3,47 @@
 		<a href="/">Morton Casa de Subastas</a>
 	</div><!-- end #logo -->
 
-	<nav class="pull-right">
-		<a href="#">Inicio</a>
-		<a href="#">Subastas</a>
-		<a href="#">Departamentos</a>
-		<a href="#">Cat&aacute;logos</a>
-		<a href="#">Nosotros</a>
-		<a href="#">Contacto</a>
+	<nav class="pull-right" role="navigation">
+
+		<!-- Collect the nav links, forms, and other content for toggling
+			<ul>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li class="divider"></li>
+						<li><a href="#">One more separated link</a></li>
+					</ul>
+				</li>
+			</ul> -->
+		<ul>
+			<li><a href="#">Inicio</a></li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Subastas</a>
+				<ul class="dropdown-menu">
+					<li><a href="#">Calendario</a></li>
+					<li><a href="#">Resultados</a></li>
+				</ul>
+			</li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Departamentos</a>
+				<ul class="dropdown-menu">
+					<li><a href="#">Antig&uuml;edades</a></li>
+					<li><a href="#">Arte Moderno</a></li>
+					<li><a href="#">Joyer&iacute;a</a></li>
+					<li><a href="#">Libros y Documentos</a></li>
+					<li><a href="#">Vinos</a></li>
+					<li class="divider"></li>
+					<li><a href="#">Oportunidades</a></li>
+					<li><a href="#">Empe&ntilde;os</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Cat&aacute;logos</a></li>
+			<li><a href="#">Nosotros</a></li>
+			<li><a href="#">Contacto</a></li>
+		</ul>	
 	</nav><!-- end .pull-right -->
 
 	<div id="quick-links">
