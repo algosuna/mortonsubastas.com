4a0ada50ca984fd5586d38db7db24b4a1d627685
Andrea <info@andyosuna.com>
2014-06-05 15:04:32 -0500

    Layout - departments
    added the whole files under the departments menu including the index.
    made a separate directory for those files to avoid clutter. includes
    are absolute paths.

---
+++ html/departamentos/arte-moderno.php
@@ -0,0 +1,122 @@
+<?php 
+//include_once 'http://mortonsubastas.andyosuna.com/php/parse-interior.php';
+//$pageId = "art";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-meta.php'; ?>
+	
+	<title>Morton Casa de Subastas - Arte Moderno</title>
+
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-css.php'; ?>
+</head>
+
+<body>
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-header.php'; ?>
+
+	<section id="banner">
+		<img src="/images/banners/dep-arte-moderno.jpg">
+	</section>
+
+	<div id="content">
+
+		<h1>Arte Moderno</h1>
+
+		<ol class="breadcrumb">
+			<li><a href="/">Inicio</a></li>
+			<li><a href="/departamentos/">Departamentos</a></li>
+			<li class="active">Arte Moderno</li>
+		</ol>
+
+		<section class="cont">
+
+			<h2 class="align-left">Descripci&oacute;n</h2>
+
+			<img src="/images/cont-sample.jpg" width="306" height="198" class="pull-right">
+			<p>Bacon ipsum dolor sit amet turkey tail pork belly, rump tenderloin ham t-bone pork. Andouille swine tail pork loin ball tip ham ham hock sirloin beef doner tri-tip. Pork loin strip steak kevin frankfurter flank.</p>
+			<p>Bacon kevin strip steak, pancetta hamburger frankfurter andouille kielbasa meatloaf jerky shoulder cow ground round prosciutto. Beef ribs sausage doner tongue chicken.</p>
+
+
+		</section>
+
+		<hr>
+
+		<section class="cont">
+
+			<h2 class="align-left">Pr&oacute;ximos Eventos</h2>
+
+			<div class="table-responsive">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>&nbsp;</th>
+							<th>Evento</th>
+							<th>Fecha</th>
+							<th>Hora</th>
+							<th>Lugar</th>
+							<th>Descripci&oacute;n</th>
+						</tr>
+					</thead><!-- end thead -->
+					<tbody>
+						<tr>
+							<td><img src="/images/upcoming/vnt-latinoamericana.jpg" width="200" height="133" alt="Subasta de Arte Latinoamericano"></td>
+							<td>Subasta de Arte Latinoamericano</td>
+							<td>Jueves, 29 de Mayo</td>
+							<td>19:00</td>
+							<td>Monte Athos 179</td>
+							<td>
+								<p>Bacon ipsum dolor sit amet sirloin salami prosciutto drumstick shank beef pastrami.</p>
+							</td>
+						</tr>
+						<tr>
+							<td><img src="/images/upcoming/vnt-latinoamericana.jpg" width="200" height="133" alt="Subasta de Arte Latinoamericano"></td>
+							<td>Subasta de Arte Latinoamericano</td>
+							<td>Jueves, 29 de Mayo</td>
+							<td>19:00</td>
+							<td>Monte Athos 179</td>
+							<td>
+								<p>Bacon ipsum dolor sit amet sirloin salami prosciutto drumstick shank beef pastrami.</p>
+							</td>
+						</tr>
+					</tbody><!-- end tbody -->
+				</table><!-- end .table -->
+			</div><!-- end .table-responsive -->
+
+		</section><!-- end .cont -->
+
+		<hr>
+
+		<section class="cont clearfix">
+
+			<h2 class="align-left">Especialistas</h2>
+
+			<div class="staff">
+				<div class="staff-box">
+					<img src="/images/staff/loin-pork.jpg" width="153" height="200">
+					<h3>Pork Loin</h3>
+					<h4>Bacon Specialist</h4>
+					<h5>10 Years</h5>
+					<p>Bacon ipsum dolor sit amet sirloin salami prosciutto drumstick shank beef pastrami chuck ball tip filet mignon corned beef pork belly bresaola pork meatloaf.</p>
+				</div><!-- end .staff-box -->
+			</div><!-- end .staff -->
+
+			<div class="staff">
+				<div class="staff-box">
+					<img src="/images/staff/salami-sirloin.jpg" width="153" height="200">
+					<h3>Sirloin Salami</h3>
+					<h4>Drumstick Specialist</h4>
+					<h5>11 Years</h5>
+					<p>Bacon ipsum dolor sit amet sirloin salami prosciutto drumstick shank beef pastrami chuck ball tip filet mignon corned beef pork belly bresaola pork meatloaf.</p>
+				</div><!-- end .staff-box -->
+			</div><!-- end .staff -->
+
+		</section><!-- end .cont -->
+
+	</div><!-- end #content -->
+
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-footer.php'; ?>
+
+    <?php include 'http://mortonsubastas.andyosuna.com/php/include-js.php'; ?>
+</body>
+</html>
