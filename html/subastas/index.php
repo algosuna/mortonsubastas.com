c0298436e005e1ce78c656819a8d550cb97ad316
Andrea <info@andyosuna.com>
2014-06-05 19:10:45 -0500

    Layout - 4
    Added new files for the rest of the interior directories following the
    example of the department directory.

---
+++ html/subastas/index.php
@@ -0,0 +1,36 @@
+<?php //require 'http://mortonsubastas.andyosuna.com/php/parse-interior.php'; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-meta.php'; ?>
+	
+	<title>Morton Casa de Subastas - Subastas</title>
+
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-css.php'; ?>
+</head>
+
+<body>
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-header.php'; ?>
+
+	<section id="banner">
+		<img src="/images/banners/dep-arte-moderno.jpg">
+	</section>
+
+	<div id="content">
+
+		<h1>Subastas</h1>
+
+		<ol class="breadcrumb">
+			<li><a href="/">Inicio</a></li>
+			<li class="active">Subastas</li>
+		</ol>
+
+		<!-- your code here! -->
+
+	</div><!-- end #content -->
+
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-footer.php'; ?>
+
+    <?php include 'http://mortonsubastas.andyosuna.com/php/include-js.php'; ?>
+</body>
+</html>
