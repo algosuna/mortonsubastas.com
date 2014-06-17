d1b32d617bb5161a3d2a0b8598312173be6aa2fe
Andrea <info@andyosuna.com>
2014-06-17 14:12:43 -0500

    nonresponsive files
    added an additional stylesheet for the static template.

---
+++ html/template-nonresponsive.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-meta.php'; ?>
+	
+	<title>Morton Casa de Subastas - Template</title>
+
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-css.php'; ?>
+	<link rel="stylesheet" type="text/css" href="/css/nonresponsive.css">
+</head>
+
+<body>
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-header.php'; ?>
+
+	<section id="banner">
+		<img src="/images/banners/dep-arte-moderno.jpg">
+	</section>
+
+	<div id="content">
+
+		<!-- your code here! -->
+
+	</div><!-- end #content -->
+
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-footer-nonresponsive.php'; ?>
+
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-js-nonresponsive.php'; ?>
+</body>
+</html>
