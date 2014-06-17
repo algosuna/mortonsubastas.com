7ad9554d5b2c00b3373a979a22c96cbfe2a65baf
Andrea <info@andyosuna.com>
2014-06-17 14:15:26 -0500

    nonresponsive - meta
    made a new include for the non responsive meta, the initial with was
    making it weird on mobile.

--- html/template-nonresponsive.php
+++ html/template-nonresponsive.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<?php include 'http://mortonsubastas.andyosuna.com/php/include-meta.php'; ?>
+	<?php include 'http://mortonsubastas.andyosuna.com/php/include-meta-nonresponsive.php'; ?>
 	
 	<title>Morton Casa de Subastas - Template</title>
 
